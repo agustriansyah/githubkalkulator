60d2fe81b8fe3848d0e87e274ab0e560988b20fb
agustriansyah <agustricip@gmail.com>
2024-08-23 11:39:31 +0800

    UBAH

--- script.js
+++ script.js
@@ -83,5 +83,5 @@ function getHistory(){
   });
   
   function printILY(){
-    document.getElementById("output-value").innerText="I LOVE U ❤";
+    document.getElementById("output-value").innerText="PLEASE DONT BE IN LOVE WITH SOMEONE ELSE! ";
   }
